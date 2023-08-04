6421a34b984a6b148dd63b0b8b899a67b9b7106f
Teresa Silverthorn <teresa.silverthorn@gmail.com>
2023-08-04 22:54:18 +0200

    Loaded in the various data sets (GHG, OM, etc.)

--- Data_analysis_Tarentaine.R
+++ Data_analysis_Tarentaine.R
@@ -1,3 +1,92 @@
 #### Data analysis and visualization of the OM and GHG dynamics in a river network (Tarentaine) impacted by damming ####
 
-#Load and merge the ancillary data, GHG flux data, OM stock and flux data, decomposition data, daily temperature data. Check for quality control 
+#Load necessary packages
+library(dplyr)
+library(tidyr)
+library(ggplot2)
+
+#Load and merge the 1. ancillary data, 2. GHG flux data, 3. OM stock and flux data, 4. leaf litter decomposition data, 5. daily temperature data. Check for quality control 
+
+#### 1. Load ancillary data ####
+
+ancil_dat <- read.csv ("C:/Users/teresa.silverthorn/Dropbox/My PC (lyp5183)/Documents/Fieldwork_2022/Data/Ancillary data/Data_entry_Tarentaine_2022_2023-08-04.csv", header=T) #Make sure it's the latest version from the Google Drive
+
+str(ancil_dat) #327 obs of 42 vars
+
+#Drop the useless columns, get the depth and velocity average per transect
+ancil_dat <- ancil_dat %>%
+  select(-CO2_start_offset_mins, -CO2_end_offset_mins, -CH4_start_offset_mins, -CH4_end_offset_mins, -ibutton_air, -ibutton_water, -X, -chamber_area, -chamber_volume, -OM_flux_net_time_mins) %>%
+  rowwise() %>%
+  mutate(ave_depth_m = mean(c_across(starts_with("depth")), na.rm = TRUE),
+         ave_velocity_m_s = mean(c_across(starts_with("velocity")), na.rm = TRUE)) %>%
+  ungroup() %>%
+  select(-starts_with("depth"), -starts_with("velocity"))
+
+#Add a "ID_unique" column with this format : "2022-05-30_TA01_1"
+
+
+
+
+############################################################################
+#### 2. Load in the GHG flux data ####
+
+GHG <- read.csv("C:/Users/teresa.silverthorn/Dropbox/My PC (lyp5183)/Documents/Fieldwork_2022/Tarentaine_GHGs/CO2.CH4.fluxes.csv")
+
+str(GHG) #323 obs. of  51 variables
+
+#Subset just the useful columns
+
+GHG <- GHG %>% 
+select(ID_unique, CO2_C_mg_m2_h, CH4_C_mg_m2_h)
+
+
+
+############################################################################
+
+#### 3. Load in the OM flux and stock data ####
+
+OM <- read.csv("C:/Users/teresa.silverthorn/Dropbox/My PC (lyp5183)/Documents/Fieldwork_2022/Data/OM_flux_stock/OM_stock_flux.csv")
+
+#Drop the useless columns
+OM <- OM %>%
+  select(-X)
+
+##########################################################################
+
+#### 4. Load in leaf litter decomposition data ####
+
+decomp <- read.csv( "C:/Users/teresa.silverthorn/Dropbox/My PC (lyp5183)/Documents/Fieldwork_2022/Data/Leaf_packs/Leaf_pack_decomposition.csv")
+
+str(decomp) #323 obs. of  20 variables
+
+#reshape the data so coarse and fine mesh LML become their own columns per site/campaign
+decomp <- decomp %>%
+  pivot_wider(names_from = Mesh_size, values_from = LML_perday) %>%
+  rename(LMLperday_coarse = Large, LMLperday_fine = Small) %>%
+  group_by(Campaign, Site) %>%   #to solve for the NA in alternating rows, group
+  summarize(LMLperday_coarse = mean(LMLperday_coarse, na.rm = TRUE),   #then take the mean
+            LMLperday_fine = mean(LMLperday_fine, na.rm = TRUE)) %>%
+  mutate(across(everything(), ~replace(., is.nan(.), NA)))    #replace NaN with NA
+
+str(decomp) #58 obs of 4 vars
+
+########################################################################
+
+#### 5. Import the daily temperature data ####
+
+# We only have site specific daily air temperature for the first campaign. 
+#we have water temperature for the leaf pack incubation period 
+
+
+air_temp <- read.csv("C:/Users/teresa.silverthorn/Dropbox/My PC (lyp5183)/Documents/Fieldwork_2022/Data/iButtons/ibutton_air_temp.csv") 
+
+str(air_temp) #327 obs. of  3 variables
+
+
+water_temp <- read.csv("C:/Users/teresa.silverthorn/Dropbox/My PC (lyp5183)/Documents/Fieldwork_2022/Data/iButtons/Daily_mean_water_temp.csv")
+
+str(water_temp) #2824 obs. of  4 variables
+
+
+###################################################################################
+
