dacd48e47a7247b473a4945b30779052cd93e307
Teresa Silverthorn <teresa.silverthorn@gmail.com>
2023-08-04 17:00:53 +0200

    New file for data analysis

---
+++ Data_analysis_Tarentaine.R
@@ -0,0 +1,3 @@
+#### Data analysis and visualization of the OM and GHG dynamics in a river network (Tarentaine) impacted by damming ####
+
+#Load and merge the ancillary data, GHG flux data, OM stock and flux data, decomposition data, daily temperature data. Check for quality control 
